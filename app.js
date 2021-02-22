9a9562651372988588f3cbd59e092bd9495aa4e5
marcrodrig <marcelo.prf9@gmail.com>
2021-02-22 10:48:34 -0300

    Cookies

--- app.js
+++ app.js
@@ -31,10 +31,11 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth (req, res, next) {
-  console.log(req.headers);
+  console.log(req.signedCookies);
+  if (!req.signedCookies.user) {
   var authHeader = req.headers.authorization;
   if (!authHeader) {
       var err = new Error('You are not authenticated!');
@@ -48,6 +49,7 @@ function auth (req, res, next) {
   var user = auth[0];
   var pass = auth[1];
   if (user == 'admin' && pass == 'password') {
+      res.cookie('user','admin',{signed: true});
       next(); // authorized
   } else {
       var err = new Error('You are not authenticated!');
@@ -55,6 +57,17 @@ function auth (req, res, next) {
       err.status = 401;
       next(err);
   }
+  }
+  else {
+    if (req.signedCookies.user === 'admin') {
+        next();
+    }
+    else {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+    }
+}
 }
 
 app.use(auth);
