9b5ee9053d3fab3f08bc7fcdce257e8c7c620d21
marcrodrig <marcelo.prf9@gmail.com>
2021-02-22 15:58:25 -0300

    Mongoose Population

--- models/dishes.js
+++ models/dishes.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+mongoose.plugin(schema => { schema.options.usePushEach = true });
 const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
@@ -14,9 +15,9 @@ var commentSchema = new Schema({
         type: String,
         required: true
     },
-    author:  {
-        type: String,
-        required: true
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
